d5e2d9cd4767d0420a7f12b6f94816bd439d66d2
suryaprakash <suryaprakashchary515@gmail.com>
2026-08-04 09:39:35 +0530

    created the frontend folder

--- services/report_service.py
+++ services/report_service.py
@@ -6,7 +6,7 @@ Service layer for report generation.
 
 from automation.report import generate_report
 
-# i have code 
+# i have code lsjdflkjsdf
 def build_report(results):
     """
     Generate validation report.
